d3b2c8c4a6dfaff43bb71343bf4fed586f0f99cd
Adesh Chandra <105627039+iadeshchandra@users.noreply.github.com>
2026-04-29 02:26:58 +0600

    Create app/src/main/java/com/trackiq/ummah/MainActivity.kt

---
+++ app/src/main/java/com/trackiq/ummah/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.trackiq.ummah
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
